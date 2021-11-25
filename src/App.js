a70c3fd11425481c1f6d06f1a4aa8f39f2dbcfbd
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-11-25 18:40:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,24 +8,9 @@ import "./assets/css/index.css";
 import "./App.css";
 
 function App() {
-	const history = useHistory();
-
-	const triggerLogin = () => {
-		const shiftStartTime = localStorage.getItem("shiftStartTime");
-
-		if (
-			shiftStartTime.slice(0, 2).toString() == new Date().getHours() &&
-			shiftStartTime.slice(3, 5).toString() == new Date().getMinutes()
-		) {
-			history.push("/login", { from: "timeTrigger" });
-		}
-	};
-
-	setTimeout(triggerLogin, 1000);
-
 	return (
 		<BrowserRouter>
-			<Routes />;
+			<Routes />
 		</BrowserRouter>
 	);
 }
