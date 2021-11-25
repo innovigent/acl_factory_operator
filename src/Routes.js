627c7fc7b3560a57deaa57e55cb4f76d619cea14
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-11-25 17:01:23 +0530

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
-
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import Changeover from "./pages/ChangeOver";
 import Downtime from "./pages/DownTime";
 import DowntimeReason from "./pages/DowntimeReason";
-import Downtimetransfer from "./pages/Downtimetransfer";
 import FaultDetection from "./pages/MachineFaultDetected";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -25,29 +22,27 @@ import SlowRunReason from "./pages/SlowRunReason";
 
 const Routes = () => {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" component={MachineLogin} />
-				<Route path="/Login" component={Login} />
-				<Route path="/Changeover" component={Changeover} />
-				<Route path="/Downtime" component={Downtime} />
-				<Route path="/FaultDetection" component={FaultDetection} />
-				<Route path="/OperatorActionTable" component={OperatorActionTable} />
-				<Route path="/ShiftChange" component={ShiftChange} />
-				<Route path="/OperatorExperiment" component={OperatorExperiment} />
-				<Route path="/SlowDownSpeed" component={SlowDownSpeed} />
-				<Route path="/Administration" component={Administration} />
-				<Route path="/StatusChanged" component={StatusChanged} />
-				<Route path="/Home" component={Home} />
-				<Route path="/VerifyIssue" component={VerifyIssue} />
-				<Route path="/DowntimeReason" component={DowntimeReason} />
-				<Route path="/SlowRunReason" component={SlowRunReason} />
-				<Route path="/Downtimetransfer" component={TransferDowntime} />
-				<Route path="/SlowSpeedTransfer" component={TransferSlowRun} />
-				<Route path="/OperatorBrakdown" component={OperatorBrakdown} />
-				<Route path="/VerifySlowdown" component={VerifySlowdown} />
-			</Switch>
-		</BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={MachineLogin} />
+			<Route path="/Login" component={Login} />
+			<Route path="/Changeover" component={Changeover} />
+			<Route path="/Downtime" component={Downtime} />
+			<Route path="/FaultDetection" component={FaultDetection} />
+			<Route path="/OperatorActionTable" component={OperatorActionTable} />
+			<Route path="/ShiftChange" component={ShiftChange} />
+			<Route path="/OperatorExperiment" component={OperatorExperiment} />
+			<Route path="/SlowDownSpeed" component={SlowDownSpeed} />
+			<Route path="/Administration" component={Administration} />
+			<Route path="/StatusChanged" component={StatusChanged} />
+			<Route path="/Home" component={Home} />
+			<Route path="/VerifyIssue" component={VerifyIssue} />
+			<Route path="/DowntimeReason" component={DowntimeReason} />
+			<Route path="/SlowRunReason" component={SlowRunReason} />
+			<Route path="/Downtimetransfer" component={TransferDowntime} />
+			<Route path="/SlowSpeedTransfer" component={TransferSlowRun} />
+			<Route path="/OperatorBrakdown" component={OperatorBrakdown} />
+			<Route path="/VerifySlowdown" component={VerifySlowdown} />
+		</Switch>
 	);
 };
 
