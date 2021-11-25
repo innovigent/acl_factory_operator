afaec92c17b324aa2e9344f838cf47418874f08a
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-11-25 17:01:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import { BrowserRouter, useHistory } from "react-router-dom";
 
-import './App.css';
-import Routes from './Routes'
-import './assets/boxicons-2.0.7/css/boxicons.min.css'
-import './assets/css/grid.css'
-import './assets/css/index.css'
+import Routes from "./Routes";
+
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./assets/css/grid.css";
+import "./assets/css/index.css";
+import "./App.css";
 
 function App() {
-  return (
-    <Routes/>
-  );
+	const history = useHistory();
+
+	const triggerLogin = () => {
+		const shiftStartTime = localStorage.getItem("shiftStartTime");
+
+		if (
+			shiftStartTime.slice(0, 2).toString() == new Date().getHours() &&
+			shiftStartTime.slice(3, 5).toString() == new Date().getMinutes()
+		) {
+			history.push("/login", { from: "timeTrigger" });
+		}
+	};
+
+	setTimeout(triggerLogin, 1000);
+
+	return (
+		<BrowserRouter>
+			<Routes />;
+		</BrowserRouter>
+	);
 }
 
 export default App;
