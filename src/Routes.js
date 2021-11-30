d6529ead331a08621cb468700d59346117f35715
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-11-30 16:45:42 +0530

    UPDATE: routes updated

--- src/Routes.js
+++ src/Routes.js
@@ -3,17 +3,10 @@ import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import Changeover from "./pages/ChangeOver";
 import Downtime from "./pages/DownTime";
 import DowntimeReason from "./pages/DowntimeReason";
-import FaultDetection from "./pages/MachineFaultDetected";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import MachineLogin from "./pages/MachineLogin";
-import OperatorActionTable from "./pages/OperatorActionTable";
-import OperatorBrakdown from "./pages/OperatorBrakdown";
-import OperatorExperiment from "./pages/OperatorExperiment";
 import SlowDownSpeed from "./pages/SlowDownSpeed";
-import StatusChanged from "./pages/StatusChanged";
-import VerifyIssue from "./pages/VerifyIssue";
-import VerifySlowdown from "./pages/VerifySlowdown";
 import Administration from "./pages/Adminstration";
 import ShiftChange from "./pages/ShiftChange";
 import TransferDowntime from "./pages/TransferDownTime";
@@ -47,21 +40,14 @@ const Routes = () => {
 			<Route path="/Login" component={Login} />
 			<Route path="/Changeover" component={Changeover} />
 			<Route path="/Downtime" component={Downtime} />
-			<Route path="/FaultDetection" component={FaultDetection} />
-			<Route path="/OperatorActionTable" component={OperatorActionTable} />
 			<Route path="/ShiftChange" component={ShiftChange} />
-			<Route path="/OperatorExperiment" component={OperatorExperiment} />
 			<Route path="/SlowDownSpeed" component={SlowDownSpeed} />
 			<Route path="/Administration" component={Administration} />
-			<Route path="/StatusChanged" component={StatusChanged} />
 			<Route path="/Home" component={Home} />
-			<Route path="/VerifyIssue" component={VerifyIssue} />
 			<Route path="/DowntimeReason" component={DowntimeReason} />
 			<Route path="/SlowRunReason" component={SlowRunReason} />
 			<Route path="/Downtimetransfer" component={TransferDowntime} />
 			<Route path="/SlowSpeedTransfer" component={TransferSlowRun} />
-			<Route path="/OperatorBrakdown" component={OperatorBrakdown} />
-			<Route path="/VerifySlowdown" component={VerifySlowdown} />
 		</Switch>
 	);
 };
