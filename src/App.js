6b1e07739f794105d0cb41a7b11335839ab851ef
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-12-02 11:38:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 
-import Routes from "./Routes";
-
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./assets/css/grid.css";
 import "./assets/css/index.css";
 import "./App.css";
+
+import Routes from "./Routes";
 
 function App() {
 	return (
