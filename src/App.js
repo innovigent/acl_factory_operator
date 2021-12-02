9598b948d9b6d853c87a3f42b9812c2c34359406
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-12-02 12:17:43 +0530

    UPDATE: all CSS files loaded into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./assets/css/chooseButton.css";
 import "./assets/css/grid.css";
 import "./assets/css/index.css";
+import "./assets/css/login.css";
+import "./assets/css/operator.css";
+import "./assets/css/user.css";
 import "./App.css";
 
 import Routes from "./routes/Routes";
